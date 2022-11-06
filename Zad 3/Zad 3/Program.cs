969092c72b5055bfe9788ac263022c898fc42d70
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2022-11-06 20:48:05 +0100

    Add files via upload

---
+++ Zad 3/Zad 3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Zad_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Podaj a");
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj b");
+
+            b = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Podaj c");
+            
+            c = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                if (a > c)
+                    Console.WriteLine("Najwieksze jest " + a);
+            }
+            
+            else if (b > a)
+            
+            {
+                if (b > c)
+                    
+                    Console.WriteLine("Najwieksze jes " + b);
+                
+                else
+                
+                    Console.WriteLine("Najwieksze jest " + c);
+                
+                Console.ReadKey();
+            }
+        }
+    }
+}
